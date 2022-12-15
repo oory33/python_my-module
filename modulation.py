3cd1873f22d340c6f1efa87aaf63a84283c5e4e6
Ryo <ryo.sblz415@gmail.com>
2022-12-15 19:50:50 +0900

    add raised cos

--- modulation.py
+++ modulation.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pyloudnorm as pyln
-from scipy.io.wavfile import write
 
 
 def sinmod(signal, srate: int, freq: float, depth: float):
@@ -21,7 +20,7 @@ def sinmod(signal, srate: int, freq: float, depth: float):
 
     Returns
     -------
-    Output signal in stereo wav flie at current directory.
+    Output signal in ndarray() .
     """
     duration = int(len(signal) / srate)
 
@@ -50,10 +49,65 @@ def sinmod(signal, srate: int, freq: float, depth: float):
     modsig_ln = pyln.normalize.loudness(mod_sig_l, lufs_sorc_l, lufs_targ)
     modsig_rn = pyln.normalize.loudness(mod_sig_r, lufs_sorc_r, lufs_targ)
 
-    mod_sig_n = np.vstack([modsig_ln, modsig_rn])
+    mod_sig_n = np.vstack([modsig_ln, modsig_rn]).T
 
-    write('SAM.wav', srate, mod_sig_n.T)
+    return mod_sig_n
 
 
 # raised-cosのOnset/Offset
-# def
+def RaisedCos(signal, srate: int, beta: float, length: float):
+    """
+    Raised-cosine window. Cuttoff would be half of the Nyquist frequency.
+
+    Parameters
+    ----------
+    signal : ndarray()
+        shape: (n,2)
+        input signal(stereo)
+    srate : int
+        sampling rate in Hz.
+    beta : float
+        window parameter.
+    length : float
+        window length in miliseconds.
+
+    Returns
+    -------
+    Output signal in ndarray().
+    """
+    lufs_targ = -14
+    meter = pyln.Meter(srate)
+
+    window_length = int(srate * length / 1000)  # in bins
+    T = 1 / window_length
+
+    a_1 = int((1-beta)/(2 * T))
+    a_2 = int((1+beta)/(2 * T))
+
+    H_f = np.zeros(window_length, dtype=float)
+
+    for i in range(a_1):
+        H_f[i] = 1
+
+    for i in range(a_1, a_2):
+        H_f[i] = 0.5 * (1 + np.cos((np.pi*T/beta) * (i - a_1)))
+
+    H_on = np.conj(np.flip(H_f))
+
+    sig_l = signal.T[0]
+    sig_r = signal.T[1]
+
+    sig_l_raise = sig_l[0:window_length] * H_on
+    sig_l_mid = sig_l[window_length:len(sig_l)-window_length]
+    sig_l_release = sig_l[len(sig_l)-window_length:len(sig_l)] * H_f
+
+    sig_r_raise = sig_r[0:window_length] * H_on
+    sig_r_mid = sig_r[window_length:len(sig_r)-window_length]
+    sig_r_release = sig_r[len(sig_r)-window_length:len(sig_r)] * H_f
+
+    sig_l = np.hstack([sig_l_raise, sig_l_mid, sig_l_release])
+    sig_r = np.hstack([sig_r_raise, sig_r_mid, sig_r_release])
+
+    out = np.vstack([sig_l, sig_r]).T
+
+    return out
