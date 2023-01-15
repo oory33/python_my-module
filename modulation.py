bbc43d5d5af225e6e96bf913847d3afbe5eb7176
Ryo <ryo.sblz415@gmail.com>
2023-01-15 18:24:11 +0900

    update

--- modulation.py
+++ modulation.py
@@ -134,7 +134,7 @@ def HalfSinMod(**kwargs):
     Output signal in ndarray().
     """
 
-    signal = np.ndarray(kwargs["signal"])
+    signal = kwargs["signal"]
     duration = int(len(signal) / kwargs["srate"])
 
     alpha = 1
